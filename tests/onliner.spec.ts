c2bb5a121f55086bd2f575ca9b8f4b2c616319cc
andreifiyas <andrewfiyas@inbox.ru>
2024-05-29 17:11:40 +0300

    Complete all 5 tests

--- tests/onliner.spec.ts
+++ tests/onliner.spec.ts
@@ -1,10 +1,14 @@
 import {expect, test} from "@playwright/test";
 import assert from "node:assert";
+import {beforeEach} from "node:test";
 
+test.beforeEach(async ({page}) => {
+    // Set timeout for this hook.
+    await page.goto("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+});
 test.describe('Bank Tests', () => {
     test('Customer login', async({page}) => {
-        await page.goto("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-        expect(page).toHaveTitle('XYZ Bank');
+        await expect(page).toHaveTitle('XYZ Bank');
         await page.getByRole('button', {name: 'Customer Login'}).click();
         const userSelect = page.locator('#userSelect');
         expect(userSelect).toBeVisible();
@@ -14,8 +18,7 @@ test.describe('Bank Tests', () => {
         await expect(page).toHaveURL('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/account')
     });
     test('Add new customer', async({page}) => {
-        await page.goto("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-        expect(page).toHaveTitle('XYZ Bank');
+        await expect(page).toHaveTitle('XYZ Bank');
         await page.getByRole('button', {name: 'Bank Manager Login'}).click();
         await expect(page.getByRole('button', {name: 'Add Customer'})).toBeVisible();
         await page.getByRole('button', {name: 'Add Customer'}).click();
@@ -24,5 +27,44 @@ test.describe('Bank Tests', () => {
         await page.getByPlaceholder('Last Name').fill('Testovich');
         await page.getByPlaceholder('Post Code').fill('192280');
         await page.locator("button[class$='btn-default']").click();
+    });
+    test('Add new deposite to custom user', async({page}) => {
+        await expect (page).toHaveTitle('XYZ Bank');
+        await page.getByRole('button', {name: 'Customer Login'}).click();
+        const userSelect = page.locator('#userSelect');
+        expect (userSelect).toBeVisible();
+        await userSelect.selectOption('2');
+        await expect (page.locator('button[ng-show^=\'custId\']', {name: "submit"})).toBeVisible();
+        await page.locator('button[ng-show^=\'custId\']', {name: "submit"}).dblclick();
+        await page.locator('button[ng-class=\'btnClass2\']').click();
+        await expect(page.getByPlaceholder('amount')).toBeVisible();
+        await page.getByPlaceholder('amount').fill('2000');
+        await page.locator('button[class$=\'btn-default\']').click();
+        await expect(page.locator('span[ng-show=\'message\']')).toBeVisible();
+    });
+    test('Add new withdrawl to custom user', async({page}) => {
+        await expect (page).toHaveTitle('XYZ Bank');
+        await page.getByRole('button', {name: 'Customer Login'}).click();
+        const userSelect = page.locator('#userSelect');
+        expect (userSelect).toBeVisible();
+        await userSelect.selectOption('2');
+        await expect (page.locator('button[ng-show^=\'custId\']', {name: "submit"})).toBeVisible();
+        await page.locator('button[ng-show^=\'custId\']', {name: "submit"}).dblclick();
+        await page.locator('button[ng-class=\'btnClass3\']').click();
+        await expect(page.getByPlaceholder('amount')).toBeVisible();
+        await page.getByPlaceholder('amount').fill('500');
+        await page.locator('button[class$=\'btn-default\']').click();
+        await expect(page.locator('span[ng-show=\'message\']')).toBeVisible();
+    });
+    test('Check user transactions', async({page}) => {
+        await expect (page).toHaveTitle('XYZ Bank');
+        await page.getByRole('button', {name: 'Customer Login'}).click();
+        const userSelect = page.locator('#userSelect');
+        expect (userSelect).toBeVisible();
+        await userSelect.selectOption('2');
+        await expect (page.locator('button[ng-show^=\'custId\']', {name: "submit"})).toBeVisible();
+        await page.locator('button[ng-show^=\'custId\']', {name: "submit"}).dblclick();
+        await page.locator('button[ng-class=\'btnClass1\']').click();
+        await expect (page.locator('a[ng-click*=\'sortReverse\']')).toBeVisible();
     })
 })
